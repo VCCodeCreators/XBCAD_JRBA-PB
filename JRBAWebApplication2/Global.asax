b6a2bbdc50a7c6f71d4ea403e648200e29f391d8
Bakhombisile02 <siyamukeladlamini1@icloud.com>
2023-09-28 07:39:45 +0000

    Add project files.

---
+++ JRBAWebApplication2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JRBAWebApplication2.MvcApplication" Language="C#" %>
---
+++ JRBAWebApplication2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JRBAWebApplication2.Startup))]
+namespace JRBAWebApplication2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
